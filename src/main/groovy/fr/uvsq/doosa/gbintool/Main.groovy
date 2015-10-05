4c779f88a7edcb09dc50c131d99efc7d1248e50d
souli <souli_bilel@hotmail.com>
2015-10-05 09:31:39 +0200

    version

---
+++ src/main/groovy/fr/uvsq/doosa/gbintool/Main.groovy
@@ -0,0 +1,12 @@
+package fr.uvsq.doosa.gbintool
+
+class Main {
+    static void main(def args) {
+        Main m = new Main()
+        println m.returnHello()
+    }
+
+    String returnHello() {
+        'Hello'
+    }
+}
